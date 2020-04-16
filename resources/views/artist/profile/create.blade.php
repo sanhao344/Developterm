3e396ac9d193546a2dd428a96e106c6133eb1cce
三好和之 <miyoshikazuyuki@miyoshikazuyukinoMacBook-puro.local>
2020-04-16 22:08:37 +0900

    artist.layout及びsidebar.layout作成、profile.createにlayout継承

--- resources/views/artist/profile/create.blade.php
+++ resources/views/artist/profile/create.blade.php
@@ -9,5 +9,24 @@
     </head>
     <body>
         <h1>プロフィール画面</h1>
+        {{-- layouts/artist.blade.phpを読み込む --}}
+        @extends('layouts.artist')
+
+
+        {{-- artist.blade.phpの@yield('title')に'プロフィール画面'を埋め込む --}}
+        @section('title', 'プロフィール画面')
+
+        {{-- artist.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+        @section('content')
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 mx-auto">
+                        <h2>プロフィール新規作成</h2>
+                    </div>
+                </div>
+            </div>
+        @endsection
+
+        @include('layout.sidebar')
     </body>
 </html>
---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,19 @@
+<!-- サイドバー -->
+<aside class="main-sidebar">
+    <section class="sidebar">
+        <ul class="sidebar-menu">
+
+            <!-- メニューヘッダ -->
+            <li class="header">カテゴリー</li>
+
+            <!-- メニュー項目 -->
+            <!-- 参照先変更するの忘れないように-->
+            <li id="painting"><a href="/admin/member/new">絵画</a></li>
+            <li id="sprayart"><a href="/admin/member/edit">スプレーアート</a></li>
+            <li id="phptograph"><a href="/admin/member/edit">写真</a></li>
+            <li id="pottery"><a href="/admin/member/edit">陶器</a></li>
+            <li id="other"><a href="/admin/member/edit">その他</a></li>
+
+        </ul>
+    </section>
+</aside><!-- end sidebar -->
