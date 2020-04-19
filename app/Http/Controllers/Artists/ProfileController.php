b4bf637b16070a1ceec1be10ece6ed6cffbdad56
三好和之 <miyoshikazuyuki@miyoshikazuyukinoMacBook-puro.local>
2020-04-19 18:09:11 +0900

    Artists/profilecontrollerにeditとupdateアクション追加

--- app/Http/Controllers/Artists/ProfileController.php
+++ app/Http/Controllers/Artists/ProfileController.php
@@ -24,7 +24,7 @@ class ProfileController extends Controller
             $profile = new Profile;
             $form = $request->all();
       
-            // フォームから画像が送信されてきたら、保存して、$news->image_path に画像のパスを保存する
+            // フォームから画像が送信されてきたら、保存して、$profile->image_path に画像のパスを保存する
             if (isset($form['image'])) {
               $path = $request->file('image')->store('public/image');
               $profile->image_path = basename($path);
@@ -44,13 +44,36 @@ class ProfileController extends Controller
         return redirect('artist/profile/create');
     }
 
-    public function edit()
+    public function edit(Request $request)
     {
-        return view('artist.profile.edit');
+        // Profile Modelからデータを取得する
+      $profile = Profile::find($request->id);
+      if (empty($profile)) {
+        abort(404);
+      }
+        return view('artist.profile.edit', ['profile_form' => $profile]);
     }
 
-    public function update()
+    public function update(Request $request)
     {
+         // Validationをかける
+      $this->validate($request, Profile::$rules);
+      // Profile Modelからデータを取得する
+      $profile = Profile::find($request->id);
+      // 送信されてきたフォームデータを格納する
+      $profile_form = $request->all();
+      if (isset($profile_form['image'])) {
+        $path = $request->file('image')->store('public/image');
+        $profile->image_path = basename($path);
+        unset($profile_form['image']);
+      } elseif (isset($request->remove)) {
+        $profile->image_path = null;
+        unset($profile_form['remove']);
+      }
+      unset($profile_form['_token']);
+      // 該当するデータを上書きして保存する
+      $profile->fill($profile_form)->save();
+
         return redirect('artist/profile/edit');
     }
 
