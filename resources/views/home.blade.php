3f6db69a38f8d778f8e739adf13e538e19172a12
Kazuyuki Miyoshi <m.sanhao@gmail.com>
2020-08-01 18:58:19 +0900

    home画面のデザイン調整

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,11 +20,29 @@
             </div>
             <div class="middle-container">
                 <div class="middle-wrapper">
-                    <h3 class="middle-message" align="center">"REART"</h3>
+                    <h3 class="middle-message" align="center">"REARTの思い"</h3>
                     <p class="middle-message-sub" align="center">
-                        "日本の伝統・文化"<br>
-                        "日本人の芸術"<br>
-                        "今こそ世界に!"
+                    人口減少、グローバル化、自国資源の不足、<br>
+                    果たして数年後、数十年後の日本はまだ先進国でいられるだろうか<br>
+                    日本が今後も国際競争力を高めて行くこと<br>
+                    それがREARTを作った理由です<br>
+                    </p>
+                </div>
+            </div>
+            <div class="middle2-container">
+                <div class="middle2-wrapper">
+                    <h3 class="middle2-message" align="center">"REARTで、できること"</h3>
+                    <h5 class="middle2-message" align="center" style="text-decoration: underline">ユーザー会員</h5>
+                    <p class="middle-message-sub2" align="center">
+                    芸術家・作品のジャンルやランキング順での検索<br>
+                    芸術作品の購入<br>
+                    お気に入りの芸術家や作品へのいいね<br>
+                    </p>
+                    <h5 class="middle2-message" align="center" style="text-decoration: underline">芸術家会員</h5>
+                    <p class="middle-message-sub2" align="center">
+                    芸術家・作品の紹介<br>
+                    芸術作品の販売<br>
+                    ランキング上位者は海外のコレクターとのコネクション支援<br>
                     </p>
                 </div>
             </div>
@@ -33,7 +51,7 @@
                 <div class="columns_container">
                     <div class="items">
                         <h3>
-                            <a href="#" target="_blank">
+                            <a class="colume" href="#" target="_blank">
                                 "Colume1 芸術との出会い"
                                 <span>- 人名 -</span>
                             </a>
@@ -42,21 +60,21 @@
                     </div>
                     <div class="items">
                         <h3>
-                            <a href="#" target="_blank">
-                                "Colume1 芸術との出会い"
+                            <a class="colume" href="#" target="_blank">
+                                "Colume2 日本の芸術の特異さ"
                                 <span>- 人名 -</span>
                             </a>
                         </h3>
-                        <div class="post_date">2020.4.24</div>                                            
+                        <div class="post_date">2020.5.30</div>                                            
                     </div>
                     <div class="items">
                         <h3>
-                            <a href="#" target="_blank">
-                                "Colume1 芸術との出会い"
+                            <a class="colume" href="#" target="_blank">
+                                "Colume3 今後の日本の芸術の向かう先"
                                 <span>- 人名 -</span>
                             </a>
                         </h3>
-                        <div class="post_date">2020.4.24</div>                                            
+                        <div class="post_date">2020.7.20</div>                                            
                     </div>
                 </div>
             </div>
